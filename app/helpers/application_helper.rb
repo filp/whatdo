35178866b89f9559d004d0f06a28ee12f57015dc
filp <dobreira@gmail.com>
2013-09-07 19:38:32 +0100

    Tweak forms and top bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def gravatar_for(email, options={})
+    email_hash = Digest::MD5.hexdigest(email)
+    image_url  = "http://www.gravatar.com/avatar/#{email_hash}"
+
+    if options[:size]
+      image_url << "?s=#{options[:size]}"
+    end
+
+    render partial: "shared/gravatar", locals: {
+      additional_classes: options[:class],
+      image_url: image_url
+    }
+  end
 end
