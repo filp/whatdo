cadd062016ef8ef3276c481d7886638a3307f84d
filp <dobreira@gmail.com>
2013-09-07 20:48:14 +0100

    Add IdeasController and Idea model

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Whatdo::Application.routes.draw do
   root "high_voltage/pages#show", id: "home"
 
   devise_for :users
+  resources  :ideas
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130907135628) do
+ActiveRecord::Schema.define(version: 20130907194751) do
+
+  create_table "ideas", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
