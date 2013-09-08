d08d7c8a2ae99f38e07ad4a002ff5e876986a2c9
filp <dobreira@gmail.com>
2013-09-08 17:54:40 +0100

    Improve User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ActiveRecord::Base
+  NAME_FORMAT = /^[A-z][A-z0-9_\ ]+$/
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, presence: true, uniqueness: true
-  validates :name,  presence: true
+  validates :name,  presence: true, length: { minimum: 4, maximum: 64 }
 
-  has_many :ideas, dependent: :destroy, counter_cache: true
+  validate :name_is_acceptable_format
+
+  def name_is_acceptable_format
+    # Validate that the display name has an acceptable format,
+    # and that it does not contain more than one space in a row.
+    unless name.match(NAME_FORMAT) && name["  "].nil?
+      errors.add(:name, "is not in an acceptable format")
+    end
+  end
 end
