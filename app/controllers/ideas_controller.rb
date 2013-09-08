ced36aea8cbf0b22d83ed87bd4cd4e4cd3cce682
filp <dobreira@gmail.com>
2013-09-08 18:07:11 +0100

    Implement form for sharing ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,6 @@
 class IdeasController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
+
   def index
     @ideas = Idea.includes(:user).paginate(page: params[:page], per_page: 10)
   end
@@ -7,5 +9,6 @@ class IdeasController < ApplicationController
   end
 
   def new
+    @idea = Idea.new
   end
 end
