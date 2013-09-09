a16978d1d058bf60cc64bab7a4946a46aa66ebd3
filp <dobreira@gmail.com>
2013-09-09 10:15:37 +0100

    Tweak markdown settings for ideas

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -7,10 +7,15 @@ class Idea < ActiveRecord::Base
 
   # This doesn't belong in the model!
   def body_html
-    Kramdown::Document.new(body).to_html
+    Kramdown::Document.new(body, markdown_options).to_html
   end
 
   def voters
     votes.up.by_type(User).voters
+  end
+protected
+  def markdown_options
+    { auto_ids: true, auto_id_prefix: "idea-", enable_coderay: true,
+      header_offset: 1 }
   end
 end
