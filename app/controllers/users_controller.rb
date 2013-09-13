89fb317a1dd755d450e6f616d0d8d14ed8e863a7
filp <dobreira@gmail.com>
2013-09-13 19:36:44 +0100

    Optimize user idea votes handling in users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user  = User.find(params[:id])
-    @ideas = @user.ideas.limit(6)
+    @ideas = @user.ideas.includes(:votes).limit(6)
   end
 end
